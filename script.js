d0ab19f60c3d2d8988884c5fac79b619ba83f47a
RealJaceKwon <dahliawxyz@gmail.com>
2025-02-12 15:23:23 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ function alertMessage() {
     const randomMessage = messages[Math.floor(Math.random() * messages.length)];
     alert(randomMessage);
 }
+
+
+let score = 0; // 초기 점수
+
+// 버튼 클릭하면 점수 증가
+document.getElementById('clickButton').addEventListener('click', function() {
+    score++;
+    document.getElementById('score').textContent = score; // 점수 업데이트
+});
